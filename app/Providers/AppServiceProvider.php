01e995d29750836bf9d509a8066c234fa46ebb0e
aleksa <onshiftmusic@gmail.com>
2020-09-22 09:08:11 +0200

    Change home routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Routing\Registrar as RegistrarContract;
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class AppServiceProvider extends BaseServiceProvider
 {
     /**
      * Register any application services.
@@ -13,7 +15,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
     }
 
     /**
@@ -23,6 +24,48 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->registerHomepageRoutes($this->app['router']);
+        $this->registerAdminRoutes($this->app['router']);
+    }
+
+    /**
+     * Registers the home page route.
+     *
+     * @param RegistrarContract $router A route registrar implementation.
+     *
+     * @return void
+     */
+    protected function registerHomepageRoutes(RegistrarContract $router): void
+    {
+        // Website homepage route.
+        $attributes = [
+            'middleware' => ['web'],
+            'namespace' => 'App\Http\Controllers\Front',
+        ];
+
+        $router->group($attributes, static function (RegistrarContract $router): void {
+            $router->get('', 'HomeController@index');
+        });
+    }
+
+    /**
+     * Registers some basic admin panel routes.
+     *
+     * @param RegistrarContract $router A route registrar implementation.
+     *
+     * @return void
+     */
+    private function registerAdminRoutes(RegistrarContract $router): void
+    {
+        $attributes = [
+            'prefix' => 'admin',
+            'middleware' => ['web', 'auth', 'permissions'],
+            'namespace' => 'App\Http\Controllers\Admin',
+        ];
+
+        $router->group($attributes, static function (RegistrarContract $router): void {
+            // admin panel home page.
+            $router->get('', 'HomeController@index');;
+        });
     }
 }
